243e3db113602f4d82c0e7e0ef4d1c4d55d37aa3
mostafa dbagh <dabagmostafa@gmail.com>
2021-01-30 09:36:35 -0800

    hello world

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -6,12 +6,12 @@ import axios from 'axios'
 const api = axios.create({
    
     baseURL:'https://novasweetcafe.herokuapp.com/api',   
-    // baseURL:'http://localhost:3005/api ',   
-  //    withCredentials: false,
-  //   headers: {
-  //     'Access-Control-Allow-Origin' : '*',
-  //     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
-  // }
+    
+     withCredentials: false,
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
+  }
 })
 
 export const insertUser = payloadd => api.post('/message',payloadd)
