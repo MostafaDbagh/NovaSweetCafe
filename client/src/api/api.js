f82881a8af986600318c215e0045824bc9a2b968
mostafa dbagh <dabagmostafa@gmail.com>
2021-01-31 22:43:01 -0800

    hello success

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 // })
 const api = axios.create({
    
-    baseURL:'http://localhost:3005/api',  
+    baseURL:'https://novasweetcafe.herokuapp.com/api',  
     
      
   //   withCredentials: false,
